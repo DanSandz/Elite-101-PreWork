094ce96ba7c5a630ceaa4eba0f5f330b4faf5009
DanSandz <110207906+DanSandz@users.noreply.github.com>
2024-01-03 22:35:12 +0000

    ChatBot Code
    This is the starter code for the chatbot project

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import random
+print("Welcome to the Elite 101 ChatBot!")
+user_name = input("What is your name? ")
+user_age = input(f"Nice to meet you, {user_name}! How old are you? ")
+print(f"Welcome {user_name}! How can I assist you today?")
 
-num_apples = random.randint(1,10)
+print("\nPlease choose from the following options: ")
+print("1: PLaceholder 1\n2: Placeholder 2\n3: Placeholder 3\n4: Exit the conversation")
 
-if num_apples >= 5:
-  print("You have more than 5 apples.")
-else:
-  print("You have less than 5 apples.")
+
+action = 0
+
+while action != "4":
+  action = input("\nEnter the number of your choice: ")
+  
+  if action == "1":
+    print("[Insert action for option 1]")
+    #code
+    #code
+    action = 0
+
+
+  elif action == "2":
+    print("option 2 was chosen!!!!!")
+    #code
+    #code
+    action = 0
+
+
+  elif action == "3":
+    print("do option 3 stuff")
+    #code
+    #code
+    action = 0
+
+
+if action == "4":
+  print(f"Goodbye {user_name}! Have a great day!")
+  quit()
